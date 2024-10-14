df162678f2da5c9c2cf9173c69eabfd58f99e4eb
Amit Sharma <amitsharma@Amits-MacBook-Pro.local>
2024-10-14 17:13:37 +0530

    cypress e2e

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    browser: {
+      enabled: true,
+      name: 'chromium',
+      provider: 'playwright',
+    },
+  },
+  // TODO: this is to make ecosystem work, remove when fixed
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+})
